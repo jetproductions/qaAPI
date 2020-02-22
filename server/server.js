9590469d474539b7deebc765c7cd1657c9fa28eb
et <eric.thomson13@gmail.com>
2020-02-22 15:12:10 -0700

    NewRelic Added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-const nr = require('newrelic/newrelic');
+const nr = require('newrelic');
 const express = require('express');
 const app = express();
-// const questions = require('./routes/questionRoutes');
-// const answers = require('./routes/answerRoutes');
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
